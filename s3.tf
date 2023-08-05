f33d7de023a6cff6320426c7584eafa59add63cb
mythiliba <mythilibala1985@gmail.com>
2023-08-05 23:07:39 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,27 +1,8 @@
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "angular-app-tfstate"
-
-  # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "hello-world-state-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+terraform {
+  backend "s3" {
+    bucket         = "angular-app-tfstate"
+    key            = "LockID"
+    region         = "us-east-2"
+    dynamodb_table = "hello-world-state-locks"
+      }
 }
