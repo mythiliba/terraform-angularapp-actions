d87d078fb6b8c48f4b5c09fc1b5d26a9a7375e49
mythiliba <mythilibala1985@gmail.com>
2023-08-05 13:13:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
  
  backend "s3" {
    bucket = "angular-app"
-   region = "us-east-2"
+   region = "us-east-1"
    key    = "sample/terraform.tfstate"
    dynamodb_table = "sample-application-terraform"
  }
