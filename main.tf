a5d8c3c1ed4338000c92e460149eb21373c76190
mythiliba <mythilibala1985@gmail.com>
2023-08-05 20:39:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,344 +1,230 @@
-# Define AWS provider
 provider "aws" {
-  region = "us-east-2"  
+  region = local.region
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "sample-application-terraform"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+data "aws_availability_zones" "available" {}
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-# Create a VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  tags = {
-    Name = "my-vpc"
-  }
-}
-
-# Create public subnet
-resource "aws_subnet" "public1" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-2a"  
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet1"
-  }
-}
-
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2b"  
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet2"
-  }
-}
-
-
-# Create security group for ecs
-resource "aws_security_group" "ecs" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ecs-security-group"
-  }
-}
-
-#create security group for alb
-resource "aws_security_group" "elb" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "alb-security-group"
-  }
-}
-
-#create a loadbalancer
-resource "aws_lb" "my_alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.elb.id]
-  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
-
-  tags = {
-    Name = "my-alb"
-  }
-}
-
-resource "aws_lb_target_group" "my_target_group" {
-  name     = "my-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
-  target_type = "ip"
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-299"
-    interval            = 30
-    timeout             = 10
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-}
-
-resource "aws_lb_listener" "my_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-  }
-}
-
-resource "aws_internet_gateway" "my_internet_gateway" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "my-internet-gateway"
-  }
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_internet_gateway.id
-  }
-
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
-resource "aws_route_table_association" "public_route_table_association1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_route_table_association" "public_route_table_association2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-
-# Create the ECS cluster
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my-ecs-cluster"  
-}
-
-
-# Create an ECR repository to store the Docker image
-resource "aws_ecr_repository" "my_ecr_repo" {
-  name = "my-angular-repo"  
-}
-
-
-# Docker image information
 locals {
-  docker_image_name = "my-angular-image"
-  docker_image_tag  = "latest"
-}
+  region = "us-east-2"
+  name   = "sample-angular"
 
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-# Build and push the Docker image to ECR
-resource "aws_ecr_lifecycle_policy" "my_ecr_lifecycle" {
-  repository = aws_ecr_repository.my_ecr_repo.name
-
-  policy = jsonencode({
-    rules = [
-      {
-        "rulePriority"      : 10,
-        "description"       : "Expire images older than 14 days",
-        "selection"         : {
-          "tagStatus"       : "tagged",
-          "countType"       : "imageCountMoreThan",
-          "countNumber"     : 10,
-          "countUnit"       : "image",
-          "sinceImagePushed": "14"
-        },
-        "action"            : {
-          "type"            : "expire"
-        }
-      }
-    ]
-  })
-}
-
-resource "docker_image" "my_angular_image" {
-  name          = myangularimage
-  build         = "./Dockerfile"  
-  registry_auth = {
-    address      = aws_ecr_repository.my_ecr_repo.repository_url
-    username     = aws.get_caller_identity.current.arn
-    password     = aws_ecr_repository.my_ecr_repo.authorization_token
-  }
-}
-
-
-resource "aws_ecs_task_definition" "my_task_definition" {
-  family                   = "angular-task"
-  execution_role_arn       = aws_iam_role.my_task_execution.arn
-  task_role_arn            = aws_iam_role.my_task.arn
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu = "256"   
-  memory = "512"  
-
-  container_definitions = <<EOF
-  [
-    {
-      "name": "local.docker_image_name",
-      "image": "docker_image.my_angular_image.latest",  
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 80,
-          "protocol": "tcp"
-        }
-      ],
-      "essential": true
-    }
-  ]
-  EOF
-}
-
-resource "aws_iam_role" "my_task_execution" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "my_task" {
-  name = "ecsTaskRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-}
-
-
-# Attach the necessary policies to the task execution role
-resource "aws_iam_role_policy_attachment" "task_execution_role_attachment" {
-  role       = aws_iam_role.my_task.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
- 
-}
-
-# Create an ECS service using Fargate launch type
-resource "aws_ecs_service" "my_service" {
-  name            = "my-ecs-service"  
-  cluster         = aws_ecs_cluster.my_cluster.arn
-  task_definition = aws_ecs_task_definition.my_task_definition.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [aws_subnet.public1.id, aws_subnet.public2.id]  
-    security_groups = [aws_security_group.ecs.id]  
-    assign_public_ip = true
- }
-
-
- load_balancer {
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-    container_name   = "angular-container"
-    container_port   = 80
-  }
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "angular-app" 
-  acl    = "private"                     
+  container_name = "sample-angular"
+  container_port = 3000
 
   tags = {
-    Name        = "My Angular Application Bucket"
-    Environment = "Development"
+    Name       = local.name
+    Example    = local.name
+    Repository = "https://github.com/terraform-aws-modules/terraform-aws-ecs"
   }
 }
 
+################################################################################
+# Cluster
+################################################################################
 
-# Export necessary values as outputs
-output "alb_dns_name" {
-  value       = aws_lb.my_alb.dns_name
-  description = "ALB DNS Name"
+module "ecs_cluster" {
+  source = "terraform-aws-modules/ecs/aws//modules/cluster"
+
+  cluster_name = local.name
+
+  # Capacity provider
+  fargate_capacity_providers = {
+    FARGATE = {
+      default_capacity_provider_strategy = {
+        weight = 50
+        base   = 20
+      }
+    }
+    
+  }
+
+  tags = local.tags
 }
 
-output "target_group_arn" {
-  value       = aws_lb_target_group.my_target_group.arn
-  description = "Target Group ARN"
+################################################################################
+# Service
+################################################################################
+
+module "ecs_service" {
+  source = "terraform-aws-modules/ecs/aws//modules/service"
+  name        = local.name
+  cluster_arn = module.ecs_cluster.arn
+
+  cpu    = 1024
+  memory = 4096
+
+  # Container definition(s)
+  container_definitions = {
+
+    fluent-bit = {
+      cpu       = 512
+      memory    = 1024
+      essential = true
+      image     = nonsensitive(data.aws_ssm_parameter.fluentbit.value)
+      firelens_configuration = {
+        type = "fluentbit"
+      }
+      memory_reservation = 50
+      user               = "0"
+    }
+
+    (local.container_name) = {
+      cpu       = 512
+      memory    = 1024
+      essential = true
+      image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
+      port_mappings = [
+        {
+          name          = local.container_name
+          containerPort = local.container_port
+          hostPort      = local.container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      # Example image used requires access to write to root filesystem
+      readonly_root_filesystem = false
+
+      dependencies = [{
+        containerName = "fluent-bit"
+        condition     = "START"
+      }]
+
+      enable_cloudwatch_logging = false
+      log_configuration = {
+        logDriver = "awsfirelens"
+        options = {
+          Name                    = "firehose"
+          region                  = local.region
+          delivery_stream         = "my-stream"
+          log-driver-buffer-limit = "2097152"
+        }
+      }
+      memory_reservation = 100
+    }
+  }
+
+  service_connect_configuration = {
+    namespace = aws_service_discovery_http_namespace.this.arn
+    service = {
+      client_alias = {
+        port     = local.container_port
+        dns_name = local.container_name
+      }
+      port_name      = local.container_name
+      discovery_name = local.container_name
+    }
+  }
+
+  load_balancer = {
+    service = {
+      target_group_arn = element(module.alb.target_group_arns, 0)
+      container_name   = local.container_name
+      container_port   = local.container_port
+    }
+  }
+
+  subnet_ids = module.vpc.private_subnets
+  security_group_rules = {
+    alb_ingress_3000 = {
+      type                     = "ingress"
+      from_port                = local.container_port
+      to_port                  = local.container_port
+      protocol                 = "tcp"
+      description              = "Service port"
+      source_security_group_id = module.alb_sg.security_group_id
+    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = local.tags
 }
 
-output "subnet_ids" {
-  value       = [aws_subnet.public1.id, aws_subnet.public2.id]
-  description = "Public Subnet IDs"
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_ssm_parameter" "fluentbit" {
+  name = "/aws/service/aws-for-fluent-bit/stable"
+}
+
+resource "aws_service_discovery_http_namespace" "this" {
+  name        = local.name
+  description = "CloudMap namespace for ${local.name}"
+  tags        = local.tags
+}
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-service"
+  description = "Service security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+
+  tags = local.tags
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = local.name
+
+  load_balancer_type = "application"
+
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.alb_sg.security_group_id]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    },
+  ]
+
+  target_groups = [
+    {
+      name             = "${local.name}-${local.container_name}"
+      backend_protocol = "HTTP"
+      backend_port     = local.container_port
+      target_type      = "ip"
+    },
+  ]
+
+  tags = local.tags
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = local.tags
 }
