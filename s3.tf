9d85464f7197e9cfa21906a33626c03f682361d1
mythiliba <mythilibala1985@gmail.com>
2023-08-07 13:37:15 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "angular-app-tfstate"
     key            = "LockID"
     region         = "us-east-2"
-    dynamodb_table = "hello-world-state-locks"
+    
       }
 }
