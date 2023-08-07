68e9669194513a727af47d41cc36344ff0a7c502
Mythili <mythili@pearlthoughts.com>
2023-08-07 12:58:26 +0530

    modified region

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "angular-app-tfstate"
     key            = "LockID"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "hello-world-state-locks"
       }
 }
